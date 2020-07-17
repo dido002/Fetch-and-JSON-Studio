fb971bd1f78c73454cfb4271946e12e487b9e8ea
Umer <an01985i@gmail.com>
2020-07-17 14:04:32 -0500

    fetch

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // TODO: add code here
+window.addEventListener('load', function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        return response.json();
+    }).then(function(jsonArray){
+        console.log("before sort:", jsonArray);
+        jsonArray.sort((a, b) => {
+            console.log(a.hoursInSpace);
+            console.log(b.hoursInSpace);
+            return (a.hoursInSpace - b.hoursInSpace);
+        })
+        console.log("after sort:", jsonArray);
+        const container = document.getElementById("container");
+        let astronauts="";
+        for (astronaut of jsonArray){
+            astronauts +=
+        `
+        <div class="astronaut">
+            <div class="bio">
+                <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                 <ul>
+                    <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                    <liclass='activeText'> active: ${astronaut.active}</li>
+                    <li>Skills: ${astronaut.skills.join(", ")}</li>
+                 </ul>
+            </div>
+            <img class="avatar" src="${astronaut.picture}">
+        </div>"
+        `
+        }
+        container.innerHTML=astronauts
+    })
+})
